355676abdda974b0cf4dd2def0b3086054c0344a
Gaurav thanvi <thanvi.gaurav@bcg.com>
2020-06-17 11:09:57 +0000

    change

--- models/CRM/Reporting/ContactDetails.sql
+++ models/CRM/Reporting/ContactDetails.sql
@@ -1,3 +1,3 @@
 {{ config(schema='CRM_Reporting') }}
 
-SELECT a.FULLNAME,a.Company,a.dob,b.Segment from {{ref('stg_vw_Contact')}} a Join {{ref('stg_vw_Segment')}} b on a.Contactid = b.Contactid
+SELECT a.FULLNAME,a.Company,a.dob,b.Segments from {{ref('stg_vw_Contact')}} a Join {{ref('stg_vw_Segment')}} b on a.Contactid = b.Contactid
